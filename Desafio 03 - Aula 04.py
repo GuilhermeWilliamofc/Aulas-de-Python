75025ca30cdc4afd8a952e8a0c383f27a7c98799
Guilherme William <desouzarodrigueswilliam@gmail.com>
2025-03-06 19:48:43 -0300

    Scripts das Aulas

---
+++ Desafio 03 - Aula 04.py
@@ -0,0 +1,5 @@
+# vamo ver no q dá
+prinum = float(input('Primeiro Número = '))
+segnum = float(input('Segundo Número = '))
+print('a soma é ', prinum + segnum)
+# fracassei, a resposta era usar 'int()' mas tô usando 'float()' pq me falaram que funciona com qualquer número fds
